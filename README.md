c7ef781d199923453300d9425588aa965036cfdf
James Young <jamesyoung3931@gmail.com>
2023-06-17 14:18:15 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # To-Do List with Django
-Created a To-Do List Application with Django. Users can create an account to create, read, update, and delete different tasks. The application is hosted on an Amazon EC2 instance using Docker, Nginx, and Gunicorn. The application data is stored on a Postgresql database. Link to project [here](#).
+Created a To-Do List Application with Django. Users can create an account to create, read, update, and delete different tasks. The application is hosted on an Amazon EC2 instance using Docker, Nginx, and Gunicorn. The application data is stored on a Postgresql database in the container. Link to project [here](#).
 
 ## Preview
 
 
 ![](./src/screenshot.png)
 
+## Structure
+
+![](./src/project-structure.jpg)
+
+The Django application is Dockerized with Gunicorn, Nginx, and Postgres with a structure similar to the diagram above. This container is deployed on an EC2 instance on AWS with a domain pointing to that instance. The container also includes Letsencrypt for SSL certificates. 
 
 ## Resources
 [Docker+Django Guide](https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/#static-files)
--- app/DjangoProject/devsettings.py
+++ app/DjangoProject/devsettings.py
@@ -21,16 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-_w4-6g6fvd$$a$0fb8!7^#aut#5el2!-i%=*+8_57(%zmp5@@x'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-_w4-6g6fvd$$a$0fb8!7^#aut#5el2!-i%=*+8_57(%zmp5@@x'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
-# DEBUG = True
+# DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+#         "USER": os.environ.get("SQL_USER", "user"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+#         "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -139,7 +139,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ['https://*', 'http://*', 'http://localhost:1337', 'http://10.15.15.214:1337']
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
