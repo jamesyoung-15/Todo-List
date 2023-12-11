26c4a4c2ed37857ba66562ff2ad48f97dde1ca9d
James Young <jamesyoung3931@gmail.com>
2023-12-11 12:07:53 +0800

    quick updates

--- app/DjangoProject/settings.py
+++ app/DjangoProject/settings.py
@@ -132,7 +132,7 @@ LOGIN_URL = 'userlogin'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
