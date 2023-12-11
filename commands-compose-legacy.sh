15a881831dddda88b6ccd4b18fb8bd1e326c4f61
James Young <jamesyoung3931@gmail.com>
2023-12-11 12:19:21 +0800

    add script

---
+++ commands-compose-legacy.sh
@@ -0,0 +1,3 @@
+sudo docker compose -f docker-compose.prod.yml exec web python manage.py migrate &&
+sudo docker compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear &&
+sudo docker compose -f docker-compose.prod.yml exec web python manage.py createsuperuser
